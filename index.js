6b361ee1096ded5a022dc6c15611e3fb962c6c76
hfog2066 <hfog2000@yahoo.com>
2023-08-28 19:31:58 -0500

    End points created

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log('My App');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello my Server in Express.');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hello, it is a new route.');
+});
+
+app.get('/productos', (req, res) => {
+  res.json([
+      {
+      name: 'Product 1',
+      price: 1000
+    },
+
+    {
+      name: 'Product 2',
+      price: 2000
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) =>{
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000
+  });
+});
+
+  app.get('/categories/:categoryId/products/:productId', (req, res) => {
+    const { categoryId, productId } = req.params;
+    res.json({
+      categoryId ,
+      productId,
+    });
+  });
+
+
+app.listen(port, () => {
+  console.log('My Port ' + port);
+});
